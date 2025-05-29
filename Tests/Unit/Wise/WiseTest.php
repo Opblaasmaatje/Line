5af05b01b58074e54cc88aca708ecffbc7a888dc
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-05-29 12:51:16 +0200

    Update classname

--- Tests/Unit/Wise/WiseTest.php
+++ Tests/Unit/Wise/WiseTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit\Wise;
 use Tests\ApplicationCase;
 
 
-class WiseCase extends ApplicationCase
+class WiseTest extends ApplicationCase
 {
     public function test_it_works()
     {
