fc2c6ca3d47b5d6fde1f9f66149df92bb4765658
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-06-02 15:43:43 +0200

    enforce no negative numbers

--- database/migrations/2025_05_30_091353_create_points_table.php
+++ database/migrations/2025_05_30_091353_create_points_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('points', function (Blueprint $table) {
             $table->id();
-            $table->integer('amount');
+            $table->unsignedBigInteger('amount');
             $table->string('source');
             $table->foreignId('account_id');
             $table->timestamps();
