dabc81c57c043a42296f30e37ee86c0fcf74ad38
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-10-01 15:10:14 +0200

    Create missing map test

---
+++ tests/Unit/Wise/Competition/DTO/CompetitionWithParticipationsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Wise\Competition\DTO;
+
+use App\Models\Competition;
+use App\Wise\Client\Endpoints\Competition\DTO\CompetitionWithParticipations;
+use Brick\JsonMapper\JsonMapper;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\ApplicationCase;
+use Tests\Unit\Wise\HasFixtureAccess;
+
+class CompetitionWithParticipationsTest extends ApplicationCase
+{
+    use HasFixtureAccess;
+
+    #[Test]
+    public function it_create_a_model()
+    {
+        $subject = $this->mapper()->map($this->getFromFixture('create_competition.json'), CompetitionWithParticipations::class);
+
+        $subject->saveModel();
+
+        $this->assertDatabaseHas(Competition::class, $subject->saveModel()->toArray());
+    }
+
+    protected function mapper(): JsonMapper
+    {
+        return $this->app->make(JsonMapper::class);
+    }
+}
