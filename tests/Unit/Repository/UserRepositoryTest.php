9e6b0f6295ed39937c38ab9998448192ed232bd4
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-10-05 16:35:49 +0200

    remove test

--- tests/Unit/Repository/UserRepositoryTest.php
+++ tests/Unit/Repository/UserRepositoryTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit\Repository;
 use App\Library\Repository\UserRepository;
 use Database\Factories\AccountFactory;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\ApplicationCase;
 
@@ -25,13 +24,5 @@ class UserRepositoryTest extends ApplicationCase
         $sut = (new UserRepository)->findAccount('some-discord-id');
 
         $this->assertEquals('this-should-be-found', $sut->username);
-    }
-
-    #[Test]
-    public function it_throws_when_user_is_not_found()
-    {
-        $this->assertThrows(function () {
-            (new UserRepository)->findAccount('this-id-does-not-exist');
-        }, ModelNotFoundException::class);
     }
 }
