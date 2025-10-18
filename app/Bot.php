906e89c9a5b74401caf3e33c67ad4bcb039f8570
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-10-18 15:33:56 +0200

    Make setting activity happen from bot start

--- app/Bot.php
+++ app/Bot.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use App\Cron\UpdateBotActivity;
 use Laracord\Laracord;
 
 class Bot extends Laracord
 {
+
+    protected function bootServices(): self
+    {
+        parent::bootServices();
+
+        UpdateBotActivity::applyActivity($this->discord(), null);
+
+        return $this;
+    }
+
     public function routes(): void
     {
         //
--- app/Cron/UpdateBotActivity.php
+++ app/Cron/UpdateBotActivity.php
@@ -3,7 +3,9 @@
 namespace App\Cron;
 
 use App\Helpers\Items;
+use Discord\DiscordCommandClient;
 use Discord\Parts\User\Activity;
+use Laracord\Console\Commands\BootCommand;
 use Laracord\Services\Service;
 
 class UpdateBotActivity extends Service
@@ -12,14 +14,21 @@ class UpdateBotActivity extends Service
 
     public function handle(): void
     {
+        self::applyActivity($this->discord(), $this->console());
+    }
+
+    public static function applyActivity(DiscordCommandClient $client, BootCommand|null $console): void
+    {
         /** @var Activity $activity */
-        $activity = $this->discord()->factory(Activity::class, [
+        $activity = $client->factory(Activity::class, [
             'type' => Activity::TYPE_GAME,
             'name' => Items::getOne()['name'],
         ]);
 
-        $this->console()->log("Updating bot activity with {$activity->name}");
+        if(! is_null($console)){
+            $console->log("Updating bot activity with {$activity->name}");
+        }
 
-        $this->discord()->updatePresence($activity);
+        $client->updatePresence($activity);
     }
 }
