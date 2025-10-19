b90a9dc22eb2e89560ceabaea1bbe20264f688b0
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-10-19 11:37:32 +0200

    Require token for API

--- app/Console/Commands/TokenMakeCommand.php
+++ app/Console/Commands/TokenMakeCommand.php
@@ -1,3 +1,4 @@
+<<<<<<< ours
 <?php
 
 namespace App\Console\Commands;
@@ -27,3 +28,28 @@ class TokenMakeCommand extends BaseTokenMakeCommand
         $this->components->bulletList([$token]);
     }
 }
+|||||||
+=======
+<?php
+
+namespace App\Console\Commands;
+
+use Laracord\Console\Commands\TokenMakeCommand as BaseTokenMakeCommand;
+
+class TokenMakeCommand extends BaseTokenMakeCommand
+{
+    protected function handleToken(bool $force = false): void
+    {
+        $tokens = $this->getTokens();
+
+        if ($tokens->isNotEmpty() && ! $force) {
+            $this->components->error("The user <fg=red>{$this->user->id}</> already has a token.");
+            return;
+        }
+
+        $token = $this->user->createToken('token', ['http'])->plainTextToken;
+
+        $this->components->bulletList([$token]);
+    }
+}
+>>>>>>> theirs
