cb45be05118e4fe4f0350baedb4c54226db7df1c
Opblaasmaatje <joris.terwolbeek@outlook.com>
2025-10-20 23:15:51 +0200

    stubout functionality

--- app/Modules/GooseBoards/Models/Enums/Status.php
+++ app/Modules/GooseBoards/Models/Enums/Status.php
@@ -4,7 +4,9 @@ namespace App\Modules\GooseBoards\Models\Enums;
 
 enum Status: string
 {
+    case IN_PROCESS = 'In process';
+
     case APPROVED = 'Approved';
+
     case REJECTED = 'Rejected';
-    case IN_PROCESS = 'In Process';
 }
