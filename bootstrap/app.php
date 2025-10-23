8954605081d40bb8e70bf2d4e970da339bf2c30d
Joris <56675005+Opblaasmaatje@users.noreply.github.com>
2025-10-23 20:14:46 +0200

    change erd generation (#85)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,18 @@
 <?php
 
-use BeyondCode\ErdGenerator\GenerateDiagramCommand;
 use Illuminate\Cache\Console\ClearCommand;
 use Illuminate\Foundation\Console\ConfigClearCommand;
 use Illuminate\Foundation\Console\StorageLinkCommand;
 use Illuminate\Foundation\Console\VendorPublishCommand;
 use LaravelZero\Framework\Application;
+use Recca0120\LaravelErd\Console\Commands\GenerateErd;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withCommands([
         StorageLinkCommand::class,
-        GenerateDiagramCommand::class,
         VendorPublishCommand::class,
         ConfigClearCommand::class,
         ClearCommand::class,
+        GenerateErd::class,
     ])
     ->create();
